73f231c0bf81555fdeb02fb236081af5c7a19d06
huynhhuuloc129 <huynhhuuloc129@gmail.com>
2024-07-25 16:36:07 +0700

    change link

--- frontend-vue/src/components/Header.vue
+++ frontend-vue/src/components/Header.vue
@@ -8,11 +8,11 @@
         </ul>
 
         <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0 w-25">
-          <li><a @click="router.push({name: 'map navigate'})" href="" class="nav-link px-2 text-white">Hiện trạng</a></li>
+          <li><a @click="router.push({name: 'map navigate'})" href="#" class="nav-link px-2 text-white">Hiện trạng</a></li>
         </ul>
 
         <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0 w-25">
-          <li><a @click="router.push({name: 'simulate'})" href="" class="nav-link px-2 text-white">Mô phỏng</a></li>
+          <li><a @click="router.push({name: 'simulate'})" href="#" class="nav-link px-2 text-white">Mô phỏng</a></li>
         </ul>
 
         <span class="text-end">
